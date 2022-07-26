9c435ceddd3a98fdc9f86eb8a38f6e376f86b8ea
MacMini <>
2022-07-26 14:13:25 -0700

    Add NaughtyAttributes

--- BeautySalon/Assets/1_Scripts/Systems/Joystick/JoystickController.cs
+++ BeautySalon/Assets/1_Scripts/Systems/Joystick/JoystickController.cs
@@ -1,11 +1,12 @@
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.EventSystems;
+
 
 namespace JoyStickSystem
 {
     public class JoystickController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
     {
-
 
         public void OnDrag(PointerEventData eventData)
         {
