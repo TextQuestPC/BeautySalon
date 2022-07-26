1deed1684db41737aa4a90137d9b5e450ce0100d
MacMini <>
2022-07-26 14:08:06 -0700

    Create joystick system

---
+++ BeautySalon/Assets/1_Scripts/Scripts/Systems/Joystick/JoystickController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace JoyStickSystem
+{
+    public class JoystickController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+    {
+
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
